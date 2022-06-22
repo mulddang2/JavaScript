21a29c1f8b321ad8f4a04d32657708cb5ffa7451
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2022-06-22 20:36:46 +0900

    [add] jQuery_기본구문
    - 제이쿼리 기본구문 연습

---
+++ jQuery/script/custom.js
@@ -0,0 +1,15 @@
+$("p").css({ display: "none" });
+
+//보이기버튼 누르면 보인다
+$(".show-btn").click(function () {
+    $("p").css({ display: "block" });
+});
+
+//감추기버튼 누르면 감춘다
+$(".hide-btn").click(function () {
+    $("p").css({ display: "none" });
+});
+
+$('선택자').click(function(){
+    
+})
