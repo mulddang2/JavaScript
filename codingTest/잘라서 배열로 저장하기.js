374ef5d02b68652d5ab37024d08d31ad6a0441ea
Suzy-Lee <mulddang2@naver.com>
2023-04-19 23:38:58 +0900

    코딩테스트 연습
    1. splice() vs slice() 차이
    2. push()
    3. join()

---
+++ codingTest/잘라서 배열로 저장하기.js
@@ -0,0 +1,12 @@
+function solution(my_str, n) {
+    let answer = [];
+    let strArr = my_str.split("");
+
+    for (let i = 0; i <= my_str.length / n; i++) {
+        let spjo = strArr.splice(0, n).join("");
+        answer.push(spjo);
+    }
+    return answer;
+}
+const result = solution("abc1Addfggg45556b", 6);
+console.log(result);
