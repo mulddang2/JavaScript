0012ea55b558493c02bfeb6b06604a2f092c5b08
Suzy-Lee <mulddang2@naver.com>
2023-08-21 22:36:26 +0900

    Study: 함수 선언문의 위험성

--- CoreJS/index.js
+++ CoreJS/index.js
@@ -1,15 +1,26 @@
 console.log(sum(1, 2))
-console.log(multiply(3, 4))
+// console.log(multiply(3, 4))
 
 // NOTE: 함수 선언문 => 함수 전체를 호이스팅함
+// NOTE: 아래에서 sum함수를 함수 선언문으로 실수로 만든 경우, 윗부분에서 숫자 값을 반환하리라 기대했지만 override.
 function sum(a, b) {
   return a + b;
 }
 
-// NOTE: 함수 표현식 => 변수 선언부만 호이스팅됨, 변수의 할당부는 원래 자리에 남겨둔다. 
-var multiply = function multiply(a, b) {
-  return a * b;
+var a = sum(1, 1)
+
+
+function sum(a, b) {
+  return a + '+' + b + '=' + (a + b);
 }
+
+var c = sum(5, 5)
+console.log(c)
+
+// NOTE: 함수 표현식 => 변수 선언부만 호이스팅됨, 변수의 할당부는 원래 자리에 남겨둔다. 
+// var multiply = function multiply(a, b) {
+//   return a * b;
+// }
 
 
 // var sum = function sum (a, b) {
@@ -22,3 +33,4 @@ var multiply = function multiply(a, b) {
 // multiply = function (a, b) {
 //   return a * b
 // }
+
