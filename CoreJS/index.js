01c8730b56162dd711cfacecfac40dfaf9d576f0
Suzy-Lee <mulddang2@naver.com>
2023-08-25 12:14:51 +0900

    Study: bind 메서드의 목적
    bind 메서드는 왜 사용할까

--- CoreJS/index.js
+++ CoreJS/index.js
@@ -1,27 +1,15 @@
-// NOTE: 1) 최대/최솟값을 구하는 코드 직접 구현
-// var numbers = [10, 20, 3, 16, 45]
-// var max = min = numbers[0]
+// NOTE: bind 메서드는 함수에 this를 미리 적용하는 것, 부분 적용 함수를 구현하는 목적 둘다 있다.
+var func = function(a, b, c, d) {
+  console.log(this, a, b, c, d) // this는 window/global이  나오겠고 1, 2, 3, 4 
+}
 
-// numbers.forEach(function(number) {
-//     if(number > max) {
-//         max = number;
-//     }
-//     if (number < min) {
-//         min = number;
-//     }
-// })
+func(1, 2, 3, 4)
 
-// console.log(max, min)
+var bindFunc1 = func.bind({ x: 1 })
+bindFunc1(5, 6, 7, 8) // { x: 1 } 5 6 7 8
 
-// NOTE: 2) call/apply 메서드 활용
-// var numbers = [10, 20, 3, 16, 45]
-// var max = Math.max.apply(null, numbers)
-// var min = Math.min.apply(null, numbers)
+// NOTE: func 에 this를 { x: 2} 로 지정하고, 인수를 4, 5로 지정함
+var bindFunc2 = func.bind({ x: 2}, 4, 5) // this 지정, 부분 적용 함수 구현
+bindFunc2(6, 7) // { x: 2 } 4 5 6 7
+bindFunc2(8, 9) // { x: 2 } 4 5 8 9
 
-// console.log(max, min)
-
-// NOTE: 3) ES6 펼치기 연산자 활용
-const numbers = [10, 20, 3, 16, 45]
-const max = Math.max(...numbers)
-const min = Math.min(...numbers)
-console.log(max, min)
