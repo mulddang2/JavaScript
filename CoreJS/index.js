0bbf635799d8337191419958cd9e86c4485ef433
Suzy-Lee <mulddang2@naver.com>
2023-08-25 12:41:59 +0900

    Study: 화살표함수에서 this
    화살표함수에서 this를 사용하면, 스코프체인상 가장 가까운 this에 접근하게 된다. 왜냐하면, 화살표함수에서는 this를 바인딩하는 과정이 없기 때문에, 함수 내부에 this가 없기 때문이다.

--- CoreJS/index.js
+++ CoreJS/index.js
@@ -1,37 +1,11 @@
-// NOTE: 상위 컨텍스트의 this를 내부함수나 콜백 함수에 전달하기
-// var obj = {
-//   outer: function() {
-//     console.log(this)
-//     var innerFunc = function (){
-//       console.log(this)
-//     }
-//     innerFunc.call(this)
-//   }
-// }
-// obj.outer()
-
-// var obj = {
-//   outer: function () {
-//     console.log(this);
-//     var innerFunc = function () {
-//       console.log(this)
-//     }.bind(this) // 내부함수에 bind로 this를 전달하면, 여기서 this는 obj를..
-//     innerFunc()
-//   },
-// };
-// obj.outer();
-
+// NOTE: 화살표함수는 실행 컨텍스트 생성 시, this 바인딩과정 제외되었기 때문에, 화살표함수에서 this는 스코프체인상 가장 가까운 this에 접근하게 된다. --> 이렇게 하면, call/apply/bind 적용할 필요가 없다.
 var obj = {
-  logThis: function () {
+  outer: function () {
     console.log(this);
-  },
-  logThisLater1: function () {
-    setTimeout(this.logThis, 500);
-  },
-  logThisLater2: function () {
-    setTimeout(this.logThis.bind(this), 500);
+    var innerFunc = () => {
+      console.log(this);
+    };
+    innerFunc();
   },
 };
-
-obj.logThisLater1() //Window {0: Window, window: Window, self: Window, document: document, name: '', location: Location, …}
-obj.logThisLater2() // obj {logThis: ƒ, logThisLater1: ƒ, logThisLater2: ƒ} 를 가리킨다.
+obj.outer();
