3839785a77f5e1bd0c700a57ae002ec6771e9ed2
Suzy-Lee <mulddang2@naver.com>
2023-08-28 10:39:40 +0900

    Study: 콜백함수 해결 - 기명함수로 변환

--- CoreJS/index.js
+++ CoreJS/index.js
@@ -1,16 +1,28 @@
-var report = {
-  sum: 0,
-  count: 0,
-  add: function () {
-    var args = Array.prototype.slice.call(arguments)
-    args.forEach(function (entry) {
-      this.sum += entry;
-      ++this.count;
-    }, this) // NOTE: 콜백 함수 내부에서의 this는 forEach 함수의 두번째 인자로 전달해준 this가 바인딩 된다. add 매서드의 this(report) 가 전달된 상태
-  },
-  average: function () {
-    return this.sum / this.count 
-  }
+// NOTE: 콜백지옥 해결 - 기명함수로 변환
+
+var coffeeList = '';
+
+var addEspresso = function (name) {
+  coffeeList = name;
+  console.log(coffeeList);
+  setTimeout(addAmericano, 3000, '아메리카노');
+};
+
+var addAmericano = function (name) {
+  coffeeList += ',' + name;
+  console.log(coffeeList);
+  setTimeout(addMocha, 3000, '카페모카');
+};
+
+var addMocha = function (name) {
+  coffeeList += ',' + name;
+  console.log(coffeeList);
+  setTimeout(addLatte, 3000, '카페라떼');
+};
+
+var addLatte = function (name) {
+  coffeeList += ',' + name;
+  console.log(coffeeList)
 }
-report.add(60, 85, 95)
-console.log(report.sum, report.count, report.average()) //240 3 80
+
+setTimeout(addEspresso, 3000, '에스프레소')
