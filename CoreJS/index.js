c048d4b16332dd118131464aea5fcacf1a348e1d
Suzy-Lee <mulddang2@naver.com>
2023-08-31 13:32:14 +0900

    Study: 클로저로 변수 보호하기

--- CoreJS/index.js
+++ CoreJS/index.js
@@ -1,22 +1,31 @@
-var addCoffee = function (name) {
-  return new Promise(function (resolve) {
-    setTimeout(function () {
-      resolve(name);
-    }, 500);
-  });
-};
-var coffeeMaker = async function () {
-  var coffeeList = '';
-  var _addCoffee = async function (name) {
-    coffeeList += (coffeeList ? ',' : '') + (await addCoffee(name));
-  };
-  await _addCoffee('에스프레소');
-  console.log(coffeeList);
-  await _addCoffee('아메리카노');
-  console.log(coffeeList);
-  await _addCoffee('카페모카');
-  console.log(coffeeList);
-  await _addCoffee('카페라떼');
-  console.log(coffeeList);
-};
-coffeeMaker();
+var createCar = function () {
+  
+  var fuel = Math.ceil(Math.random() * 10 + 10) // 연료
+  var power = Math.ceil(Math.random() * 3 + 2) // 연비
+  var moved = 0;
+
+  var publicMembers = {
+    get moved() {
+      return moved;
+    },
+    run: function() {
+      var km = Math.ceil(Math.random() * 6)
+      var wasteFuel = km / power;
+      if (fuel < wasteFuel) {
+        console.log('이동불가')
+        return
+      }
+      fuel -= wasteFuel;
+      moved += km;
+      console.log(km + 'km 이동 (총 '+ moved + 'km). 남은 연료: ' + fuel)
+    }
+  }
+  Object.freeze(publicMembers)
+  return publicMembers;
+}
+var car = createCar()
+car.run()
+console.log(car.fuel)
+
+car.fuel = 1000
+console.log(car.fuel)
