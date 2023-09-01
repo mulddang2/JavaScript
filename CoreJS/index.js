a9022f0c01f88115000ed9032acbe284454499d3
Suzy-Lee <mulddang2@naver.com>
2023-09-01 11:54:41 +0900

    Study: 클로저 활용_ 디바운스

--- CoreJS/index.js
+++ CoreJS/index.js
@@ -1,55 +1,26 @@
-// TODO: 다시 공부해보기 : 이게 왜 클로저 정의에 부합한지 더 공부..
+/**
+ * NOTE:
+ * 디바운스: 짧은 시간 동안 동일한 이벤트가 많이 발생할 경우, 이를 전부처리하지 않고, 처음 또는 마지막에 발생한 이벤트에 대해 한번만 처리하는 것 --> FE 성능 최적화에 도움을 준다
+ *          
+ */
 
-Object.defineProperty(global, '_', {
-  value: 'EMPTY_SPACE',
-  writable: false,
-  configurable: false,
-  enumerable: false,
-});
-
-var partial2 = function () {
-  var originalPartialArgs = arguments;
-  var func = originalPartialArgs[0];
-  if (typeof func !== 'function') {
-    throw new Error('첫 번째 인자가 함수가 아닙니다.');
+var debounce = function (eventName, func, wait) {
+  var timeoutId = null;
+  return function (event) {
+    var self = this;
+    console.log(eventName, 'event 발생')
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(func.bind(self, event), wait)
   }
+}
 
-  return function () {
+var moveHandler = function (e) {
+  console.log('move event 처리')
+}
 
-    var partialArgs = Array.prototype.slice.call(originalPartialArgs, 1);
-      // NOTE: Array.prototype.slice : begin 부터 end 까지 얕은 복사로 배열객체를 반환함
-      // NOTE: Function.prototype.call() 메서드를 사용해서 바인딩할 수 있다.
-      // slice를 통해서 새로운 Array를 변환하기 위하여 Function.prototype.call()으로 바인딩을 해주어야 한다.
+var wheelHandler = function (e) {
+  console.log('wheel event 처리')
+}
 
-
-    // NOTE: Array.prototype을 [] 로 간단하게 쓸수 있음
-    var restArgs = [].slice.call(arguments);
-
-    // 처음에 넘겨준 인자들 중, _로 비워놓은 공간마다, 나중에 넘어온 인자들이 차례대로 끼워짐
-    for (var i = 0; i < partialArgs.length; i++) {
-      if (partialArgs[i] === _) {
-        partialArgs[i] = restArgs.shift();
-      }
-    }
-    return func.apply(this, partialArgs.concat(restArgs));
-  };
-};
-
-var add = function () {
-  var result = 0;
-  for (var i = 0; i < arguments.length; i++) {
-    result += arguments[i];
-  }
-  return result;
-};
-
-var addPartial = partial2(add, 1, 2, _, 4, 5, _, _, 8, 9);
-console.log(addPartial(3, 6, 7, 10));
-
-var dog = {
-  name: '강아지',
-  greet: partial2(function (prefix, suffix) {
-    return prefix + this.name + suffix;
-  }, '왈왈, '),
-};
-dog.greet(' 배고파요!');
+document.body.addEventListener('mousemove', debounce('move', moveHandler, 500))
+document.body.addEventListener('mousewheel', debounce('wheel', moveHandler, 700))
