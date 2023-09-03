7121239feb1171cf0c26aa03f9ac85b8ece21fe8
Suzy-Lee <mulddang2@naver.com>
2023-09-03 10:53:56 +0900

    Study: 클로저 활용_ 커링함수

--- CoreJS/index.js
+++ CoreJS/index.js
@@ -1,26 +1,21 @@
 /**
- * NOTE:
- * 디바운스: 짧은 시간 동안 동일한 이벤트가 많이 발생할 경우, 이를 전부처리하지 않고, 처음 또는 마지막에 발생한 이벤트에 대해 한번만 처리하는 것 --> FE 성능 최적화에 도움을 준다
- *          
+ * NOTE: 커링..?
+ * 1. 한번에 하나의 인자만 전달한다.
+ * 2. 마지막 인자가 전달되기전 까지는 원본 함수가 실행되지 않는다.
  */
 
-var debounce = function (eventName, func, wait) {
-  var timeoutId = null;
-  return function (event) {
-    var self = this;
-    console.log(eventName, 'event 발생')
-    clearTimeout(timeoutId)
-    timeoutId = setTimeout(func.bind(self, event), wait)
-  }
-}
+var curry3 = function (func) {
+  return function (a) {
+    return function (b) {
+      return func(a, b);
+    };
+  };
+};
 
-var moveHandler = function (e) {
-  console.log('move event 처리')
-}
+var getMaxWith10 = curry3(Math.max)(10)
+console.log(getMaxWith10(8))
+console.log(getMaxWith10(25))
 
-var wheelHandler = function (e) {
-  console.log('wheel event 처리')
-}
-
-document.body.addEventListener('mousemove', debounce('move', moveHandler, 500))
-document.body.addEventListener('mousewheel', debounce('wheel', moveHandler, 700))
+var getMinWith10 = curry3(Math.min)(10)
+console.log(getMinWith10(8))
+console.log(getMinWith10(25))
